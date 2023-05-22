4c2760d6d3e93c58f36130e7913a43619bf7fdd3
Luis David Garcia Lunar <ldgl1215@gmail.com>
2023-05-22 16:44:46 -0300

    best user experience

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -9,6 +9,8 @@ import {
   FormControl,
   Input,
   Button,
+  Spinner,
+  Center,
 } from "native-base"
 import {FlatList} from "react-native-gesture-handler"
 import {Feather, AntDesign} from "@expo/vector-icons"
@@ -20,6 +22,8 @@ import {Pressable} from "react-native"
 import {ScreenStackProps} from "react-native-screens"
 import {RootStackScreenProps} from "../navigation/RootStack"
 import {baseUrl} from "../common/constants"
+import {Ionicons} from "@expo/vector-icons"
+
 type Client = {
   name: string
   cpf: string
@@ -29,7 +33,7 @@ type Client = {
 export const Home = ({navigation}: RootStackScreenProps<"Home">) => {
   const [currentClient, setCurrentClient] = useState<Client | null>(null)
   const {isOpen, onOpen, onClose} = useDisclose()
-  const {data} = useQuery<Client[]>("clients", async () => {
+  const {data, isLoading, isError, refetch} = useQuery<Client[]>("clients", async () => {
     const result = await axios.get(baseUrl + "/api/clients")
     return result.data
   })
@@ -38,13 +42,35 @@ export const Home = ({navigation}: RootStackScreenProps<"Home">) => {
     setCurrentClient(client)
     onOpen()
   }
+  if (isError) {
+    return (
+      <>
+        <Center flex={1}>
+          <Text fontSize={20}>Erro ao carregar os dados</Text>
+          <Button
+            onPress={() => refetch()}
+            mt={4}
+            rightIcon={<Icon as={Ionicons} name="reload" />}
+          >
+            Tentar novamente
+          </Button>
+        </Center>
+      </>
+    )
+  }
 
   return (
     <View flex={1} my={4} position={"relative"}>
-      <FlatList
-        data={data}
-        renderItem={({item}) => <ListItem item={item} modalTrigger={openClientDetail} />}
-      ></FlatList>
+      {isLoading ? (
+        <Spinner flex={1} size={64} />
+      ) : (
+        <FlatList
+          data={data}
+          renderItem={({item}) => (
+            <ListItem item={item} modalTrigger={openClientDetail} />
+          )}
+        ></FlatList>
+      )}
       <Fab
         position="absolute"
         bgColor={"primary.500"}
@@ -56,45 +82,47 @@ export const Home = ({navigation}: RootStackScreenProps<"Home">) => {
         icon={<Icon color="white" as={<AntDesign name="plus" />} size={8} />}
       ></Fab>
 
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <Modal.Content>
-          <Modal.Header
-            _text={{
-              textAlign: "center",
-            }}
-          >
-            Detalhe do cadastro
-          </Modal.Header>
-          <Modal.Body>
-            <View alignItems={"center"} fontSize={24}>
-              <Image
-                alt="client image"
-                size={64}
-                resizeMode={"contain"}
-                source={{uri: baseUrl + currentClient?.imageUrl}}
-              ></Image>
-              <Text fontWeight={"medium"} my={1} fontSize={20}>
-                {currentClient?.name}
-              </Text>
-              <Text fontSize={16} color={"gray.500"}>
-                CPF: {currentClient?.cpf}
-              </Text>
-            </View>
-            {/* close button */}
-            <Button
-              onPress={onClose}
-              mt={5}
-              mx={3}
-              bgColor={"primary.500"}
+      {currentClient && (
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <Modal.Content>
+            <Modal.Header
               _text={{
-                color: "white",
+                textAlign: "center",
               }}
             >
-              Fechar
-            </Button>
-          </Modal.Body>
-        </Modal.Content>
-      </Modal>
+              Detalhe do cadastro
+            </Modal.Header>
+            <Modal.Body>
+              <View alignItems={"center"} fontSize={24}>
+                <Image
+                  alt="client image"
+                  size={64}
+                  resizeMode={"contain"}
+                  source={{uri: baseUrl + currentClient?.imageUrl}}
+                ></Image>
+                <Text fontWeight={"medium"} my={1} fontSize={20}>
+                  {currentClient?.name}
+                </Text>
+                <Text fontSize={16} color={"gray.500"}>
+                  CPF: {currentClient?.cpf}
+                </Text>
+              </View>
+              {/* close button */}
+              <Button
+                onPress={onClose}
+                mt={5}
+                mx={3}
+                bgColor={"primary.500"}
+                _text={{
+                  color: "white",
+                }}
+              >
+                Fechar
+              </Button>
+            </Modal.Body>
+          </Modal.Content>
+        </Modal>
+      )}
     </View>
   )
 }
@@ -106,7 +134,7 @@ const ListItem = ({
   item: Client
   modalTrigger: (client: Client) => void
 }) => {
-  //console.log("render", item)
+  console.log("render", item)
   return (
     <Pressable onPress={() => modalTrigger(item)}>
       <View
